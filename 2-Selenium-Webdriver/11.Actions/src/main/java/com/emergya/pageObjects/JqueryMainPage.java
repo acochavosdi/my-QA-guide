5c7981d2a3cfdda821331ec4bda1e30e5fc9ad39
Carlos Ochavo <acochavosdi@gmail.com>
2018-04-06 15:05:33 +0200

    Creada la Rama de qa

--- 2-Selenium-Webdriver/11.Actions/src/main/java/com/emergya/pageObjects/JqueryMainPage.java
+++ 2-Selenium-Webdriver/11.Actions/src/main/java/com/emergya/pageObjects/JqueryMainPage.java
@@ -2,8 +2,6 @@ package com.emergya.pageObjects;
 
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
 
 import com.emergya.selenium.drivers.EmergyaWebDriver;
 import com.emergya.selenium.pageObject.BasePageObject;
@@ -27,8 +25,6 @@ public class JqueryMainPage extends BasePageObject {
 	 */
 
 	private static final String INTRO_PARAGRAPH_XPATH = "introSelectorXpath";
-	private static final String LINK_TO_DRAGGABLE_PAGE_XPATH = "linkToDraggablePageXpath";
-	private static final String BOOKS_SPAN_XPATH = "spanBooksXPATH";
 
 	/**
 	 * Constructor
@@ -89,115 +85,6 @@ public class JqueryMainPage extends BasePageObject {
 		log.info("[log-PageObjects] " + this.getClass().getSimpleName() + " - End isReadyWithEmergyaHandler method");
 
 		return status;
-	}
-
-	public String getClassOfTheParagraphIntro() {
-
-		if (this.isElementVisibleByXPath(INTRO_PARAGRAPH_XPATH)) {
-			WebElement introParagraph = this.getElementByXPath(INTRO_PARAGRAPH_XPATH);
-			return introParagraph.getAttribute("class");
-		}
-
-		return "bad request";
-	}
-
-	/**
-	 * Go to the Draggable Page.Include Steps:
-	 * 
-	 * - Esperar hasta que algo sea visible, máximo 10s
-	 * 
-	 * - Comprobar que ya es visible.
-	 * 
-	 * - Almacenar el elemento en un WebElement
-	 * 
-	 * - Imprimir por log.info() el valor del atributo 'href' del link
-	 * 
-	 * - Esperar hasta que sea clickable, máximo 10s
-	 * 
-	 * - Comprobar que ya es clickable.
-	 * 
-	 * - Clickar en el link.
-	 * 
-	 * - Comprobar que la página Draggable esta ready.
-	 */
-	public JqueryDraggablePage goToDraggablePage() {
-
-		WebElement element;
-		String hrefAttributeOfTheElement;
-
-		/*
-		 * CUANDO NO USAMOS METODOS DEL HANDLER (this.$loquesea), NOS VEMOS OBLIGADOS A
-		 * USAR LA FUNCION getXPath PARA EL SELECTOR, YA QUE AUNQUE NO USEMOS EL
-		 * HANDLER, EL VERDADERO SELECTOR ESTÁ EN EL FICHERO PROPERTIES, AL QUE SOLO
-		 * ACCEDEN LOS METODOS DEL HANDLER, SI NO USARAMOS ESTE METODO, EN VEZ DE
-		 * ENVIARLE A LOS METODOS EL SELECTOR, LE ENVIARIAMOS EL VALOR DE LA CONSTANTE
-		 * DEFINIDA AL PRINCIPIO DE LA CLASE
-		 * 
-		 * 
-		 * LAS LINEAS COMENTADAS SON EQUIVALENTES A SU SIGUIENTE LINEA,PERO USANDO EL
-		 * HANDLER U OTRO MÉTODO VÁLIDO
-		 */
-
-		// this.waitForByXPath(LINK_TO_DRAGGABLE_PAGE_XPATH, 10);
-		driver.waitUntilVisible(By.xpath(getXPath(LINK_TO_DRAGGABLE_PAGE_XPATH)), 10);
-
-		// if(this.isElementVisibleByXPath(LINK_TO_DRAGGABLE_PAGE_XPATH))
-		if (driver.isElementDisplayed(By.xpath(getXPath(LINK_TO_DRAGGABLE_PAGE_XPATH)))) {
-
-			// element = this.getElementByXPath(LINK_TO_DRAGGABLE_PAGE_XPATH);
-			element = driver.findElement(By.xpath(getXPath(LINK_TO_DRAGGABLE_PAGE_XPATH)));
-			hrefAttributeOfTheElement = element.getAttribute("href");
-			log.info("Href attribute of the element is : " + hrefAttributeOfTheElement);
-
-			if (driver.waitUntilElementClickable(By.xpath(getXPath(LINK_TO_DRAGGABLE_PAGE_XPATH)), 10)) {
-
-				element.click();
-
-				// driver.clickIfExists(By.xpath(getXPath(LINK_TO_DRAGGABLE_PAGE_XPATH)));
-
-				// driver.clickOnWithMouse(By.xpath(getXPath(LINK_TO_DRAGGABLE_PAGE_XPATH)));
-
-				return new JqueryDraggablePage(driver);
-
-			}
-		}
-
-		return null;
-	}
-
-	public void usingScrolling() throws InterruptedException {
-
-		
-
-
-
-		// Scroll to Bottom
-		this.scrollBottom();
-		Thread.sleep(1500);
-
-		// Scroll to Top
-		this.scrollTop();
-		Thread.sleep(1500);
-
-		// Scroll to a WebElement
-		this.scrollTo(this.getElementByXPath(BOOKS_SPAN_XPATH));
-		Thread.sleep(1500);
-
-		// Scroll with Javascript
-		JavascriptExecutor jse = (JavascriptExecutor) driver;
-
-		jse.executeScript("window.scrollBy(0,250)", "");
-		Thread.sleep(1500);
-
-		// Scroll to Bottom
-		this.scrollBottom();
-		Thread.sleep(1500);
-
-		jse.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-		Thread.sleep(1500);
-
-		this.scrollTop();
-		Thread.sleep(1500);
 	}
 
 }
