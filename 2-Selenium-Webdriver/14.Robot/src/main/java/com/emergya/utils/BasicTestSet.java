33b92cd931399a2c757431e562cd9c0249eceabb
Carlos Ochavo <acochavosdi@gmail.com>
2018-04-09 09:50:28 +0200

    Finished 14.Robot, TODO documentation + comments

---
+++ 2-Selenium-Webdriver/14.Robot/src/main/java/com/emergya/utils/BasicTestSet.java
@@ -0,0 +1,9 @@
+package com.emergya.utils;
+
+import com.emergya.pageObjects.DemoAutomationTestingRegisterPage;
+import com.emergya.selenium.testSet.DefaultTestSet;
+
+public abstract class BasicTestSet extends DefaultTestSet {
+
+	protected DemoAutomationTestingRegisterPage demoAutomationTestingRegisterPage;
+}
